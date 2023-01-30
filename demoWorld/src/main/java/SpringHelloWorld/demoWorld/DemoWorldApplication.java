a4a230add3e04b42d33acaff98216a71be97ddfe
prathyushapalisetti <prathyusha2001sai@gmail.com>
2023-01-30 20:36:03 +0530

    added

---
+++ demoWorld/src/main/java/SpringHelloWorld/demoWorld/DemoWorldApplication.java
@@ -0,0 +1,13 @@
+package SpringHelloWorld.demoWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWorldApplication.class, args);
+	}
+
+}
